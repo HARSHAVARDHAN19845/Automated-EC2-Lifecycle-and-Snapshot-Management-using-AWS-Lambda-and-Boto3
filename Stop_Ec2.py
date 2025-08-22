096ae6a08ca2f15b3d1ca574626af0bb169140cf
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2025-08-22 14:01:38 +0530

    Update Stop_Ec2.py

--- Stop_Ec2.py
+++ Stop_Ec2.py
@@ -1,8 +1,9 @@
 import boto3
-region = 'us-west-1'
+region = 'us-west-1' #select region where the instance is currently running
 instances = ['i-12345cb6de4f78g9h', 'i-08ce9b2d7eccf6d26']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
     ec2.stop_instances(InstanceIds=instances)
-    print('stopped your instances: ' + str(instances))
+    print('stopped your instances: ' + str(instances))  #used for logging
+
